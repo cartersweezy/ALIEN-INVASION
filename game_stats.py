a1d3fcaae24514d9fecc9162a9384847f80ecc31
cartersweezy <samp03donat@yandex.ru>
2024-01-17 15:01:42 +0100

    add

--- game_stats.py
+++ game_stats.py
@@ -5,15 +5,12 @@ class GameStats():
         '''Инициализирует статистику'''
         self.settings = ai_game.settings
         self.reset_stats()
+
+        # Игра Alien Invasion запускается в неактивном состоянии
+        self.game_active = False
         
         # Рекорд не должен сбрасываться
         self.high_score = 0
-        
-        # Игра Alien Invasion запускается в активном состоянии
-        self.game_active = True
-        
-        # Игра Alien Invasion запускается в неактивном состоянии
-        self.game_active = False
         
     def reset_stats(self):
         '''Инициализирует статистику, изменяющуюся в ходе игры'''
--- scoreboard.py
+++ scoreboard.py
@@ -18,27 +18,10 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         
-    def prep_score(self):
-        '''Преобразует текущий счет в графическое изображение'''
-        rounded_score = round(self.stats.score, -1)
-        score_str = '{:,}'.format(rounded_score)
-        self.score_image = self.font.render(score_str, True,
-                                            self.text_color, self.settings.bg_color)
-        
-        # Вывод счет в правой верхней части экрана
-        self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
-        
-    def show_score(self):
-        '''Выводит счет на экран'''
-        self.screen.blit(self.score_image, self.score_rect)
-        self.screen.blit(self.high_score_image, self.high_score_rect)
         
     def prep_high_score(self):
         '''Преобразует рекордный счет в графическое изображение'''
-        high_score = round(self.stats.high_score, -1)
-        high_score_str = '{:,}'.format(high_score)
+        high_score_str = str(self.stats.high_score)
         self.high_score_image = self.font.render(high_score_str, True,
                                                  self.text_color, self.settings.bg_color)
         
@@ -46,9 +29,27 @@ class Scoreboard():
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
+        
+    def prep_score(self):
+        '''Преобразует текущий счет в графическое изображение'''
+        rounded_score = round(self.stats.score, -1)
+        score_str = f"{rounded_score:,}"
+        self.score_image = self.font.render(score_str, True,
+                                            self.text_color, self.settings.bg_color)
+        
+        # Вывод счет в правой верхней части экрана
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
     
+    def show_score(self):
+        '''Выводит счет на экран'''
+        self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        
     def check_high_score(self):
         '''Проверяет, появился ли новый рекорд'''
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+            
